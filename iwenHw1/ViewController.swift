a5d81477d575c354526ed4dd1328dd1c03fc9c95
user_12 <user_12@mac-12deimac.local>
2018-03-29 13:33:04 +0800

    Initial Commit

---
+++ iwenHw1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iwenHw1
+//
+//  Created by user_12 on 2018/3/29.
+//  Copyright © 2018年 user12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
